1679d41c738e213561945554be3f977541fafa36
Samantha <imadragonhearmerawr@gmail.com>
2019-05-09 17:36:10 -0700

    Felt like making a react frontend, will delete later, idk

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Container, Header, Segment, Button, Icon, Dimmer, Loader, Divider } from 'semantic-ui-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {}
+    this.getRecipes = this.getRecipes.bind(this)
+    this.getRecipe = this.getRecipe.bind(this)
+  }
+
+  componentDidMount () {
+    this.getRecipes()
+  }
+
+  fetch (endpoint) {
+    return window.fetch(endpoint)
+      .then(response => response.json())
+      .catch(error => console.log(error))
+  }
+
+  getRecipes () {
+    this.fetch('/api/recipes')
+      .then(recipes => {
+        if (recipes.length) {
+          this.setState({recipes: recipes})
+          this.getRecipe(recipes[0].id)
+        } else {
+          this.setState({recipes: []})
+        }
+      })
+  }
+
+  getRecipe (id) {
+    this.fetch(`/api/recipes/${id}`)
+      .then(recipe => this.setState({recipe: recipe}))
+  }
+
+  render () {
+    let {recipes, recipe} = this.state
+    return recipes
+      ? <Container text>
+        <Header as='h2' icon textAlign='center' color='teal'>
+          <Icon name='unordered list' circular />
+          <Header.Content>
+            List of Ingredients
+          </Header.Content>
+        </Header>
+        <Divider hidden section />
+        {recipes && recipes.length
+          ? <Button.Group color='teal' fluid widths={recipes.length}>
+            {Object.keys(recipes).map((key) => {
+              return <Button active={recipe && recipe.id === recipes[key].id} fluid key={key} onClick={() => this.getRecipe(recipes[key].id)}>
+                {recipes[key].title}
+              </Button>
+            })}
+          </Button.Group>
+          : <Container textAlign='center'>No recipes found.</Container>
+        }
+        <Divider section />
+        {recipe &&
+          <Container>
+            <Header as='h2'>{recipe.title}</Header>
+            {recipe.description && <p>{recipe.description}</p>}
+            {recipe.ingredients &&
+              <Segment.Group>
+                {recipe.ingredients.map((ingredient, i) => <Segment key={i}>{ingredient.description}</Segment>)}
+              </Segment.Group>
+            }
+            {recipe.steps && <p>{recipe.steps}</p>}
+            {recipe.source && <Button basic size='tiny' color='teal' href={recipe.source}>Source</Button>}
+          </Container>
+        }
+      </Container>
+      : <Container text>
+        <Dimmer active inverted>
+          <Loader content='Loading' />
+        </Dimmer>
+      </Container>
+  }
 }
 
-export default App;
+export default App
